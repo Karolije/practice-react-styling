48275e2b79a2d4f47b7bb047325aca0c2c8659a0
Karolina <karolina.jedrajczyk@gmail.com>
2025-06-10 10:45:06 +0200

    add task 01 and 02

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,20 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+const backgroundColors = {
+  primary: () => "#cce5ff",
+  secondary: () => "#e2e3e5",
+};
 
 const StyledAlert = styled.div`
-    display: block;
-`
+  display: block;
+  color: #004085;
+  margin-bottom: 1rem;
+  border-color: #b8daff;
+  border-radius: 0.25rem;
+  padding: 0.75rem 1.25rem;
+  border: 1px solid transparent;
+  background-color: ${({ variant, theme }) =>
+    backgroundColors[variant]?.(theme)};
+`;
 
 export { StyledAlert };
---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const backgroundColors = {
+  primary: (theme) => theme?.colors?.primary || "#007bff",
+  secondary: (theme) => theme?.colors?.secondary || "#e2e3e5",
+};
+const sizes = {
+  lg: (theme) => theme?.buttonPadding?.lg || "0.5rem 1rem",
+  sm: (theme) => theme?.buttonPadding?.sm || "0.5rem 0.25rem",
+};
+const StyledButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  padding: ${({ size, theme }) => sizes[size]?.(theme)};
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+  border-radius: 0.3rem;
+  color: #fff;
+  border-color: #007bff;
+  background-color: ${({ variant, theme }) =>
+    backgroundColors[variant]?.(theme)};
+
+  ${({ active }) =>
+    active &&
+    `
+    box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+    transform: translateY(1px);
+  `}
+
+  ${({ disabled }) =>
+    disabled &&
+    `
+    opacity: 0.65;
+    pointer-events: none;
+  `}
+`;
+
+export { StyledButton };
